bf88ae4fe263a9051be6ff40bc0774b791a99b79
Wizard of z/OS <wizard@zdevops.com>
2021-11-27 23:39:54 +0100

    More nice text

--- endpoints/d01p1rexx.py
+++ endpoints/d01p1rexx.py
@@ -8,7 +8,13 @@ from subprocess import Popen, PIPE
 
 #  Restful way of creating APIs through Flask Restful
 class REXXD01P1(MethodResource, Resource):
-    @doc(description="Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.", tags=['REXX'],
+    @doc(description="Before you leave, the Elves in accounting just need you to fix your expense report" +
+    "(your puzzle input); apparently, something isn't quite adding up." +
+    "Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers" + 
+    "together. For example, suppose your expense report contained the following:" + 
+    "1721,979,366,299,675,1456. In this list, the two entries that sum to 2020 are 1721 and 299." +
+    "Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579."
+    , tags=['REXX'],
     responses={
         '200': {'description': 'Everything is ok!'},
     }
